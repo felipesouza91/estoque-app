6ec0b23b831e51f6fe2c8062185fbae8dc497dd4
Felipe Souza <felipedb91@gmail.com>
2023-09-08 21:59:41 -0300

    feat: update home layou

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { BorrowingItem } from '../../components/BorrowingItem'
 import { Container } from './styles'
 
+const list = [1, 2, 3, 4, 5, 6, 7, 8]
+
 const Home: React.FC = () => {
   return (
     <Container>
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,3 +1,7 @@
 import { SafeAreaView } from 'react-native-safe-area-context'
 import styled from 'styled-components/native'
-export const Container = styled(SafeAreaView)``
+export const Container = styled(SafeAreaView)`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors['gray-700']};
+  padding: 0 24px;
+`
