2f83b13c646cbbcbadf9bf8c595588cdd1d5a0ef
Felipe Souza <felipedb91@gmail.com>
2023-09-09 22:42:40 -0300

    feat: create button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
-import { Container } from './styles'
-
-const Button: React.FC = () => {
-  return <Container></Container>
+import { TouchableOpacityProps } from 'react-native/types'
+import { Container, Title } from './styles'
+interface IButtonProps extends TouchableOpacityProps {
+  title: string
+  type?: 'default' | 'cancel'
+}
+const Button: React.FC<IButtonProps> = ({
+  title,
+  type = 'default',
+  ...rest
+}) => {
+  return (
+    <Container type={type} {...rest}>
+      <Title>{title}</Title>
+    </Container>
+  )
 }
 
 export default Button
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,3 +1,23 @@
 import styled from 'styled-components/native'
 
-export const Container = styled.View``
+interface IContainerProps {
+  type?: 'default' | 'cancel'
+}
+
+export const Container = styled.TouchableOpacity.attrs(({ theme }) => ({
+  activeOpacity: 0.5,
+}))<IContainerProps>`
+  height: 56px;
+  max-height: 56px;
+  padding: 16px 24px;
+  background-color: ${({ theme, type }) =>
+    type === 'cancel' ? theme.colors['red-500'] : theme.colors['green-700']};
+  align-items: center;
+  justify-content: center;
+  border-radius: 6px;
+`
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.colors.white};
+  font-size: ${({ theme }) => theme.fontSize.lg}px;
+  font-family: ${({ theme }) => theme.fontFamily.bold};
+`
