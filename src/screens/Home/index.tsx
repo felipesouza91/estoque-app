ba26acbd2e5f142c4f7496ae55a97a328ecb56da
Felipe Souza <felipedb91@gmail.com>
2023-09-09 22:46:52 -0300

    fix: update header in home page

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import { BorrowingItem } from '../../components/BorrowingItem'
+import Header from '../../components/Header'
 import { BorrowingList, Container, ItemSeparator } from './styles'
 
 const list = [1, 2, 3, 4, 5, 6, 7, 8]
 
 const Home: React.FC = () => {
   return (
-    <Container>
-      <BorrowingList
-        data={list}
-        renderItem={(item) => <BorrowingItem />}
-        ItemSeparatorComponent={() => <ItemSeparator />}
-      />
-    </Container>
+    <>
+      <Header />
+      <Container>
+        <BorrowingList
+          data={list}
+          renderItem={(item) => <BorrowingItem />}
+          ItemSeparatorComponent={() => <ItemSeparator />}
+        />
+      </Container>
+    </>
   )
 }
 
