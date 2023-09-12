c516096abcda7d77bfac3e62268810108b389678
Felipe Souza <felipedb91@gmail.com>
2023-09-11 23:02:56 -0300

    feat: implenets basic oauth code flow

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,31 +1,59 @@
+import { AxiosError } from 'axios'
 import * as AuthSession from 'expo-auth-session'
 import * as WebBrowser from 'expo-web-browser'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { api } from '../../api'
 import Button from '../../components/Button'
 import { Input } from '../../components/Input'
 import { Container, Title } from './styles'
 WebBrowser.maybeCompleteAuthSession()
 const useProxy = true
-const redirectUri = AuthSession.makeRedirectUri()
+const redirectUri = AuthSession.makeRedirectUri({
+  scheme: 'apptecredirect',
+})
 console.log(redirectUri)
 const SignIn: React.FC = () => {
   // Create and load an auth request
   const [request, result, promptAsync] = AuthSession.useAuthRequest(
     {
+      responseType: 'code',
       clientId: 'angular',
+      clientSecret: 'password',
+      state: '123456789',
       redirectUri,
       scopes: ['READ', 'WRITE'],
-      usePKCE: true,
-      responseType: 'code',
-      state: '123456789',
-      codeChallenge: '12345689',
+      codeChallengeMethod: AuthSession.CodeChallengeMethod.S256,
     },
     {
       authorizationEndpoint: 'http://192.168.0.113:8080/oauth2/authorize',
       tokenEndpoint: 'http://192.168.0.113:8080/oauth2/token',
     },
   )
-  console.log(JSON.stringify(result, null, 2))
+
+  async function loadToken(code: string, codeVerifier) {
+    try {
+      const { data } = await api.post(
+        '/oauth2/token',
+        `grant_type=authorization_code&code=${code}&redirect_uri=${redirectUri}&code_verifier=${codeVerifier}`,
+        {
+          headers: {
+            Authorization: 'Basic YW5ndWxhcjpwYXNzd29yZA==',
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        },
+      )
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+      const erroAxios = error as AxiosError
+    }
+  }
+  useEffect(() => {
+    if (result?.type === 'success') {
+      console.log(result)
+      loadToken(result.params.code, request.codeVerifier)
+    }
+  }, [result])
   return (
     <Container>
       <Title>Acesse sua conta</Title>
