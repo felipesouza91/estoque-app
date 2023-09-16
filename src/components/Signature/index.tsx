ca643584101c8fdc5800b99425d0392cf94b2696
Felipe Souza <felipedb91@gmail.com>
2023-09-15 22:25:53 -0300

    feat: implements components like a modal

--- src/components/Signature/index.tsx
+++ src/components/Signature/index.tsx
@@ -1,11 +1,25 @@
 import * as FileSystem from 'expo-file-system'
 import React, { useRef } from 'react'
+import { ModalProps } from 'react-native'
 import SignatureLib, { SignatureViewRef } from 'react-native-signature-canvas'
-
 import Button from '../Button'
-import { ButtonContainer, Container } from './styles'
+import {
+  ButtonContainer,
+  Container,
+  Header,
+  HeaderText,
+  ModalContainer,
+} from './styles'
 
-const Signature: React.FC = () => {
+type ISignatureProps = ModalProps & {
+  onComplete: (signatureUri?: string) => void
+}
+
+const Signature: React.FC<ISignatureProps> = ({
+  onComplete,
+
+  ...rest
+}) => {
   const ref = useRef<SignatureViewRef>()
 
   const handleOK = (signature) => {
@@ -16,31 +30,37 @@ const Signature: React.FC = () => {
       { encoding: FileSystem.EncodingType.Base64 },
     )
       .then(() => FileSystem.getInfoAsync(path))
-      .then(console.log)
+      .then((value) => {
+        onComplete(value.uri)
+      })
       .catch(console.error)
   }
 
   const handleClear = () => {
     ref.current.clearSignature()
+    onComplete('')
   }
 
   const handleConfirm = () => {
     ref.current.readSignature()
   }
   const handleEmpty = () => {
-    console.log('Empty')
+    onComplete('')
   }
 
   return (
-    <Container>
-      <SignatureLib ref={ref} onOK={handleOK} onEmpty={handleEmpty} rotated />
-      {
+    <ModalContainer {...rest}>
+      <Container>
+        <Header>
+          <HeaderText>Assinatura do técnico</HeaderText>
+        </Header>
+        <SignatureLib ref={ref} onOK={handleOK} onEmpty={handleEmpty} rotated />
         <ButtonContainer>
-          <Button title="Salvar" onPress={handleConfirm} />
+          <Button title="Continuar" onPress={handleConfirm} />
           <Button title="Cancelar" type="cancel" onPress={handleClear} />
         </ButtonContainer>
-      }
-    </Container>
+      </Container>
+    </ModalContainer>
   )
 }
 
--- src/components/Signature/styles.ts
+++ src/components/Signature/styles.ts
@@ -1,7 +1,19 @@
-import { SafeAreaView } from 'react-native-safe-area-context'
 import styled from 'styled-components/native'
 
-export const Container = styled(SafeAreaView)`
+export const ModalContainer = styled.Modal``
+
+export const Header = styled.View`
+  padding: 20px 0;
+  align-items: center;
+  justify-content: center;
+`
+
+export const HeaderText = styled.Text`
+  font-family: ${({ theme }) => theme.fontFamily.bold};
+  font-size: ${({ theme }) => theme.fontSize.xl}px;
+`
+
+export const Container = styled.View`
   flex: 1;
 `
 
