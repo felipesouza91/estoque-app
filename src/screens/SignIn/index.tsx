4a0d3ed35f43ec7fba8221bf9f95773638726c12
Felipe Souza <felipedb91@gmail.com>
2023-09-20 23:52:10 -0300

    refactor: refactor auth in signin page

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,47 +1,19 @@
-import * as AuthSession from 'expo-auth-session'
-import * as WebBrowser from 'expo-web-browser'
-import React, { useEffect } from 'react'
+import React from 'react'
 import Button from '../../components/Button'
 import { Input } from '../../components/Input'
 import { useAuth } from '../../hooks/useAuth'
 import { Container, Title } from './styles'
-WebBrowser.maybeCompleteAuthSession()
-const redirectUri = AuthSession.makeRedirectUri({
-  scheme: 'apptecredirect',
-})
-const SignIn: React.FC = () => {
-  const { user, loadTokenWithCode } = useAuth()
-  const [request, result, promptAsync] = AuthSession.useAuthRequest(
-    {
-      responseType: 'code',
-      clientId: 'angular',
-      clientSecret: 'password',
-      state: '123456789',
-      redirectUri,
-      scopes: ['READ', 'WRITE'],
-      codeChallengeMethod: AuthSession.CodeChallengeMethod.S256,
-    },
-    {
-      authorizationEndpoint: 'http://192.168.0.113:8080/oauth2/authorize',
-      tokenEndpoint: 'http://192.168.0.113:8080/oauth2/token',
-    },
-  )
 
-  useEffect(() => {
-    if (result?.type === 'success') {
-      loadTokenWithCode(result.params.code, request.codeVerifier, redirectUri)
-    }
-  }, [result])
+const SignIn: React.FC = () => {
+  const { user, login } = useAuth()
+  async function handleLogin() {
+    await login()
+  }
   return (
     <Container>
       <Title>Acesse sua conta</Title>
       <Input placeholder="Ip/Dominio do servidor" icon="server" />
-      <Button
-        title="Entrar"
-        onPress={() => {
-          promptAsync({})
-        }}
-      />
+      <Button title="Entrar" onPress={handleLogin} />
     </Container>
   )
 }
