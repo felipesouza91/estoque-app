084a64609c738a8a890ab38f1147284c4f03796a
Felipe Souza <felipedb91@gmail.com>
2023-09-26 22:54:51 -0300

    feat: use text field

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,19 +1,45 @@
+import { yupResolver } from '@hookform/resolvers/yup'
 import React from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import * as yup from 'yup'
 import Button from '../../components/Button'
 import { Input } from '../../components/Input'
 import { useAuth } from '../../hooks/useAuth'
 import { Container, Title } from './styles'
 
+const schema = yup.object({
+  serverUrl: yup.string().required('Campo obrigatório'),
+})
+
 const SignIn: React.FC = () => {
-  const { user, login } = useAuth()
-  async function handleLogin() {
-    await login()
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  })
+  const { login } = useAuth()
+  async function handleLogin(data) {
+    await login(data.serverUrl)
   }
   return (
     <Container>
       <Title>Acesse sua conta</Title>
-      <Input placeholder="Ip/Dominio do servidor" icon="server" />
-      <Button title="Entrar" onPress={handleLogin} />
+      <Controller
+        control={control}
+        name="serverUrl"
+        render={({ field: { onBlur, onChange, value } }) => (
+          <Input
+            placeholder="Ip/Dominio do servidor"
+            icon="server"
+            value={value}
+            onChangeText={onChange}
+            errorMessage={errors.serverUrl?.message}
+          />
+        )}
+      />
+      <Button title="Entrar" onPress={handleSubmit(handleLogin)} />
     </Container>
   )
 }
