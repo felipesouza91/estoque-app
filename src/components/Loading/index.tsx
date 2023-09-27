14bdfcc72b7e872e64f020a83544c7656e891392
Felipe Souza <felipedb91@gmail.com>
2023-09-26 21:25:58 -0300

    feat: create loading page

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, Rounded } from './styles'
+
+const Loading: React.FC = () => {
+  return (
+    <Container>
+      <Rounded />
+    </Container>
+  )
+}
+
+export default Loading
