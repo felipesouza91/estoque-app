5578b6ce81c83011aa377857d815eaa95d9cde6b
Felipe Souza <felipedb91@gmail.com>
2023-09-27 20:52:46 -0300

    feat: add url_storage key

--- src/config/storageConfig.ts
+++ src/config/storageConfig.ts
@@ -1,3 +1,4 @@
 const USER_STORAGE = '@app-tec:user'
 const TOKEN_STORAGE = '@app-tec:token'
-export { USER_STORAGE, TOKEN_STORAGE }
+const URL_STORAGE = '@app-tec:url'
+export { USER_STORAGE, TOKEN_STORAGE, URL_STORAGE }
